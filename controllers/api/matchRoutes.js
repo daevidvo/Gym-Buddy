68940f32779a37bbdf5ee60cf913f86e0c8f87d3
Kaiden Parcher <ktparcher@.com>
2023-05-08 13:40:56 -0700

    merge

--- controllers/api/matchRoutes.js
+++ controllers/api/matchRoutes.js
@@ -1,10 +1,24 @@
-const router = require('express').Router()
-const {User, Messages, Matches} = require('../../models')
+const router = require('express').Router();
+const { User, Matches, } = require('../../models');
 
-// router.get('/', )
+// Get all matches
+router.get('/', async (req, res) => {
+  try {
+    const matchData = await Matches.findAll();
+    res.status(200).json(matchData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+router.post('/', async (req, res) => {
+    try {
+      const newMatch = await Matches.create(req.body);
+      res.status(200).json(newMatch);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
 
+module.exports = router;
 
-
-
-module.exports = router
