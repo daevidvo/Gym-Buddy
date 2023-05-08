1fbcf4007667c9ac55163b17d7c0ab67f6714e8f
Kaiden Parcher <ktparcher@.com>
2023-05-08 11:46:58 -0700

    add comments

--- models/Matches.js
+++ models/Matches.js
@@ -25,11 +25,12 @@ Matches.init(
         }
     },
     {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'Matches',
+        
+        sequelize, // Pass the sequelize connection to the Matches model
+        freezeTableName: true, // Set the table name to match the model name
+        underscored: true, // Use underscores instead of camelCase in column names
+        modelName: 'Matches', // Set the model name
     }
 );
 
-module.exports = Matches;
+module.exports = Matches; // Export the Matches model
