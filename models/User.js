a8ca838d4c2027c632d5f5b7f565e2ad53b37810
Kaiden Parcher <ktparcher@.com>
2023-05-08 11:53:02 -0700

    finish model comments

--- models/User.js
+++ models/User.js
@@ -3,12 +3,14 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+// Define User model
 class User extends Model {
     checkPassword(loginPw) {
       return bcrypt.compareSync(loginPw, this.password);
     }
   }
   
+  // Define User fields and validations
 User.init(
     {
         id: {
@@ -43,14 +45,14 @@ User.init(
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [50,260],
+                len: [50,260], // bio must be between 50 and 260 characters long
             },
         },
         training_goals: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [10,75],
+                len: [10,75], // training goals must be between 10 and 75 characters long
             },
         },
         age: {
@@ -58,11 +60,12 @@ User.init(
             allowNull: false,
             validate: {
                 isNumeric: true,
-                min: 18,   
+                min: 18,   // age must be at least 18
             },
         },
     },
     {
+       // Define hooks to hash password before creating/updating user
         hooks: {
             beforeCreate: async (newUserData) => {
               newUserData.password = await bcrypt.hash(newUserData.password, 10);
@@ -82,4 +85,5 @@ User.init(
     }
 );
 
+// Export User model
 module.exports = User;
