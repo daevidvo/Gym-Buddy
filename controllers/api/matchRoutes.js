a643e550c91b9e253d9a5cc77e1c44f5a907044e
Kaiden Parcher <ktparcher@.com>
2023-05-08 13:46:23 -0700

    update match route

--- controllers/api/matchRoutes.js
+++ controllers/api/matchRoutes.js
@@ -1,24 +1,39 @@
 const router = require('express').Router();
-const { User, Matches, } = require('../../models');
+const { User, Matches } = require('../../models');
 
 // Get all matches
 router.get('/', async (req, res) => {
+    try {
+      // Get the current user's ID from their session
+      const currentUserId = req.session.userId;
+      
+      // Find all matches where the current user is either user1Id or user2Id
+      const matchData = await Matches.findAll({
+        where: {
+          [Op.or]: [
+            { user1Id: currentUserId },
+            { user2Id: currentUserId }
+          ]
+        }
+      });
+      
+      res.status(200).json(matchData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+// Create a new match
+router.post('/', async (req, res) => {
   try {
-    const matchData = await Matches.findAll();
+    const userId = req.session.userId; // Get the user ID from session
+    const matchData = await Matches.create({ ...req.body, userId }); // Add user ID to new match
     res.status(200).json(matchData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.post('/', async (req, res) => {
-    try {
-      const newMatch = await Matches.create(req.body);
-      res.status(200).json(newMatch);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-});
-
 module.exports = router;
+
 
