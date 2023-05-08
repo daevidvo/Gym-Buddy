29cb23eb427ffd54d69453c820ba52dc7fd6ca04
Kaiden Parcher <ktparcher@.com>
2023-05-08 14:00:22 -0700

    continue on routes

--- controllers/api/matchRoutes.js
+++ controllers/api/matchRoutes.js
@@ -6,22 +6,34 @@ router.get('/', async (req, res) => {
     try {
       // Get the current user's ID from their session
       const currentUserId = req.session.userId;
-      
-      // Find all matches where the current user is either user1Id or user2Id
+      // Get all matches where the current user is either user1Id or user2Id
       const matchData = await Matches.findAll({
         where: {
           [Op.or]: [
             { user1Id: currentUserId },
             { user2Id: currentUserId }
           ]
-        }
+        },
+        include: [
+          {
+            model: User,
+            as: 'user1',
+            attributes: ['id', 'username', 'bio']
+          },
+          {
+            model: User,
+            as: 'user2',
+            attributes: ['id', 'username', 'bio']
+          }
+        ]
       });
-      
       res.status(200).json(matchData);
     } catch (err) {
       res.status(500).json(err);
     }
   });
+  
+
 
 // Create a new match
 router.post('/', async (req, res) => {
