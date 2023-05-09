9e8917ee78aa4c7f01e15e65e853f3d4bc44cf07
daevidvo <itsdavidvo@gmail.com>
2023-05-09 14:36:26 -0700

    changed homeroutes to html folder

---
+++ controllers/html/profileRoutes.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const { User, Messages, Matches } = require("../../models");
+const withAuth = require("../../utils/auth.js");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: {
+        exclude: ["password"],
+      },
+    });
+
+    const currentUser = userData.get({ plain: true });
+
+    res.render("profile", {
+      logged_in: req.session.logged_in,
+      currentUser,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
