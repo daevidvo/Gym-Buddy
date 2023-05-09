b18f3d1b0a05d87ec980f5cfda70cb99d46a23a3
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-05-09 12:14:28 -0700

    added more handlebar files, added signup formhandler, and filled out some handlebar pages

--- public/js/login.js
+++ public/js/login.js
@@ -22,36 +22,9 @@ const loginFormHandler = async (event) => {
     }
   };
   
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-    const age = document.querySelector('#age-singup')
-    const bio = document.querySelector('#bio-signup').value.trim();
-    const training_goals = document.querySelector('#training-goals-signup').value.trim();
-    
-    if (name && email && password && bio && training_goals && age) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, email, password, age, bio, training_goals }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/homepage');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
-  
   document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
   
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+
   
