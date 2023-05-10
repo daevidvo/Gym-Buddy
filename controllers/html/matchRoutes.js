7aafdffe80f6a33b01ca40c27177b694399e3352
Kaiden Parcher <ktparcher@.com>
2023-05-10 10:50:10 -0700

    updateing get routes

--- controllers/html/matchRoutes.js
+++ controllers/html/matchRoutes.js
@@ -1,19 +1,21 @@
 const router = require('express').Router();
 const { Matches, User } = require('../../models');
 const withAuth = require('../../utils/auth');
+const { Op } = require('sequelize');
 
 router.get('/', withAuth, async (req, res) => {
   try {
     const userMatches = await Matches.findAll({
       where: { 
         [Op.or]: [
-          { user1Id: req.session.user_id },
-          { user2Id: req.session.connect_id }
+            console.log(Matches),
+          { user_id: req.session.user_id },
+          { connect_id: req.session.connect_id }
         ]
       },
       include: [{ model: User, as: 'user_id' }, { model: Matches, as: 'connect_id' }]
     });
-
+    console.log(userMatches)
     res.render('matches', { logged_in: true, userMatches });
   } catch (err) {
     res.status(500).json(err);
