dd6b72438fd7bec5f4767f4550ef88062bbe36e3
Kaiden Parcher <ktparcher@.com>
2023-05-11 19:15:07 -0700

    commenting

--- controllers/html/matchRoutes.js
+++ controllers/html/matchRoutes.js
@@ -1,11 +1,11 @@
-const router = require("express").Router();
-const { Matches, User } = require("../../models");
-const withAuth = require("../../utils/auth");
-const { Op } = require("sequelize");
+const router = require("express").Router(); // import Router from express to handle routes
+const { Matches, User } = require("../../models"); // import Matches and User models from the models folder
+const withAuth = require("../../utils/auth"); // import the withAuth middleware from the utils folder
+const { Op } = require("sequelize"); // import Op from sequelize to handle complex queries
 
 router.get("/", withAuth, async (req, res) => {
   try {
-    const userMatches = await Matches.findAll({
+    const userMatches = await Matches.findAll({ // find all matches that include the logged-in user
       where: {
         [Op.or]: [
           { user_id: req.session.user_id },
@@ -14,17 +14,17 @@ router.get("/", withAuth, async (req, res) => {
       },
       include: [
         {
-          model: User,
+          model: User, // include the User model in the query to get user details
           attributes: {
-            exclude: ["password", "email"],
+            exclude: ["password", "email"], // exclude sensitive attributes from the user details
           },
         },
       ],
     });
 
-    const matchData = JSON.parse(JSON.stringify(userMatches, null, 2));
+    const matchData = JSON.parse(JSON.stringify(userMatches, null, 2)); // convert the data to JSON format
 
-    for (let x = 0; x < matchData.length; x += 1) {
+    for (let x = 0; x < matchData.length; x += 1) { // iterate through the matches and remove the current user's match
       if (req.session.user_id == matchData[x].user_id) {
         matchData.splice(x, 1);
         // must do x-=1 because we're changing the length of matchData
@@ -32,32 +32,32 @@ router.get("/", withAuth, async (req, res) => {
       }
     }
 
-    res.render("matches", { logged_in: true, matchData });
+    res.render("matches", { logged_in: true, matchData }); // render the matches page with the matchData
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err); // handle any errors that occur during the retrieval of matches
   }
 });
 
 router.get("/:id", async (req, res) => {
   try {
-    const matchData = await Matches.findOne({where: {id: req.params.id}})
-    const match = matchData.get({plain: true})
+    const matchData = await Matches.findOne({where: {id: req.params.id}}) // find the match with the given id
+    const match = matchData.get({plain: true}) // get the match data in plain format
 
-    const user1Data = await User.findByPk(match.user_id, {
+    const user1Data = await User.findByPk(match.user_id, { // find the first user in the match
         attributes: {
-            exclude: ['email', 'password']
+            exclude: ['email', 'password'] // exclude sensitive attributes from the user details
         }
     })
     const user1 = user1Data.get({plain: true})
     
-    const user2Data = await User.findByPk(match.connect_id, {
+    const user2Data = await User.findByPk(match.connect_id, { // find the second user in the match
         attributes: {
             exclude: ['email', 'password']
         }
     })
-    const user2 = user2Data.get({plain: true})
+    const user2 = user2Data.get({plain: true}) // get the second user data in plain format
 
-    res.render("messages", {
+    res.render("messages", { // render the messages page with the user1 and user2 details
         user2: user2.userName,
         user1: user1.userName,
         logged_in: req.session.logged_in
--- controllers/html/profileRoutes.js
+++ controllers/html/profileRoutes.js
@@ -1,17 +1,21 @@
+// modules and imports
 const router = require("express").Router();
 const { User, Matches } = require("../../models");
 const withAuth = require("../../utils/auth.js");
 
+// Route for displaying the user's profile
 router.get("/", withAuth, async (req, res) => {
   try {
+    // Query the database for the user's data, excluding the password
     const userData = await User.findByPk(req.session.user_id, {
       attributes: {
         exclude: ["password"],
       },
     });
 
+    // Extract the plain object version of the user's data
     const currentUser = userData.get({ plain: true });
-
+    // Render the profile template with the user's data
     res.render("profile", {
       logged_in: req.session.logged_in,
       currentUser,
@@ -20,20 +24,23 @@ router.get("/", withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+// Route for displaying the login page
 router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect to the homepage
   if (req.session.logged_in) {
     res.redirect("/");
     return;
   }
-
+  // Render the login template
   res.render("login");
 });
-
+// Route for displaying the signup page
 router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect to the homepage
   if (req.session.logged_in) {
     res.redirect('/')
   } else {
+    // Render the signup template
     res.render('signup', {
       logged_in: req.session.logged_in
     })
